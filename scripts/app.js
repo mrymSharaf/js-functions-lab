da63d459da9192f630b75687ae5f5e797dd201c6
Mariam Sharaf <maryamshaider25@gmail.com>
2025-07-02 20:11:19 +0300

    submit js functions lab

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,13 @@
 //2
 function isAdult(age) {
     if (age < 18) {
-        console.log('Minor')
+        return 'Minor'
     }
     else if (age >= 18) {
-        console.log('Adult')
+        return 'Adult'
     }
     else {
-        console.log('Invalid age')
+        return 'Invalid age'
     }
 }
 
@@ -17,10 +17,10 @@ console.log('Exercise 2 Result:', isAdult(21));
 function isCharAVowel(char) {
     char = char.toLowerCase();
     if (char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u') {
-        console.log('true');
+        return 'true'
     }
     else {
-        console.log('false');
+        return 'false'
     }
 }
 
@@ -41,13 +41,13 @@ function greetUser(name, time) {
     // name = charAt(0).toUpperCase()
     // time = charAt(0).toUpperCase()
     if (time === 'morning') {
-        console.log('Good morning ' + name);
+        return 'Good morning ' + name
     }
     else if (time == - 'afternoon') {
-        console.log('Good afternoon ' + name);
+        return 'Good afternoon ' + name
     }
     else if (time === 'evening') {
-        console.log('Good evening ' + name);
+        return 'Good evening ' + name
     }
     else {
         console.log('invalid input')
@@ -59,40 +59,39 @@ console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
 function maxOfThree(num1, num2, num3) {
 
     if (num1 >= num2 && num1 >= num3) {
-        console.log(num1);
+        return num1
     }
     else if (num2 >= num1 && num2 >= num3) {
-        console.log(num2);
+        return num2
     }
     else if (num3 >= num1 && num3 >= num2) {
-        console.log(num3);
+        return num3
     }
     else {
-        console.log('Invalid input');
+        return 'Invalid input'
     }
 }
 console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
 
 //7
-function calculateTip(bill, tipPercentage){
+function calculateTip(bill, tipPercentage) {
     const tipAmount = bill * (tipPercentage / 100)
     return tipAmount
 }
 console.log('Exercise 7 Result:', calculateTip(50, 20));
 
 //8
-function convertTemperature(temp, scale){
+function convertTemperature(temp, scale) {
     if (scale === 'C' || scale === 'c') {
-        const fahrenheit = (temp * 9/5) + 32;
-        return fahrenheit;
+        const fahrenheit = (temp * 9 / 5) + 32;
+        return fahrenheit
     }
-    else if (scale === 'F' || scale === 'f'){
-        const celsius = (temp - 32) * 5/9;
-        return celsius;
+    else if (scale === 'F' || scale === 'f') {
+        const celsius = (temp - 32) * 5 / 9;
+        return celsius
     }
-    else{
-        console.log('Invalid scale');
-        return null;
+    else {
+        return 'Invalid scale'
     }
 }
 console.log('Exercise 8 Result:', convertTemperature(32, "C"));
